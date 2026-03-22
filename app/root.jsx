df3fdc8df3740af6d590ca66c4503430ec3978f6
yassinehaa <hayassine0@gmail.com>
2026-03-22 17:45:26 +0100

    fixing vercel

--- app/root.jsx
+++ app/root.jsx
@@ -136,3 +136,21 @@ export function ErrorBoundary() {
     </html>
   );
 }
+
+export function HydrateFallback() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#111" />
+        <meta name="color-scheme" content="dark light" />
+        <Meta />
+        <Links />
+      </head>
+      <body data-theme="dark">
+        <Scripts />
+      </body>
+    </html>
+  );
+}
